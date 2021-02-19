f1ad45b7f0c36972ef4e015ceaad4d23ebacc427
John H. Hartman <john.hartman@gmail.com>
2021-02-19 14:21:14 -0700

    cleanup

--- phase1d/phase1d.c
+++ phase1d/phase1d.c
@@ -22,7 +22,9 @@ startup(int argc, char **argv)
     P1CondInit();
 
     // initialize device data structures
-    // put DeviceHandler into interrupt vector for the devices
+    USLOSS_IntVec[USLOSS_CLOCK_INT] = DeviceHandler;
+    // put DeviceHandler into interrupt vector for the remainingdevices
+    
     USLOSS_IntVec[USLOSS_SYSCALL_INT] = SyscallHandler;
 
     /* create the sentinel process */
@@ -72,9 +74,8 @@ Sentinel (void *notused)
     rc = P1_Fork("P2_Startup", P2_Startup, NULL, 4 * USLOSS_MIN_STACK, 2, &pid);
     assert(rc == P1_SUCCESS);
 
-    // enable interrupts
     // while sentinel has children
-    //      use P1GetChildStatus to get children that have quit 
+    //      call P1GetChildStatus to get children that have quit 
     //      wait for an interrupt via USLOSS_WaitInt
     USLOSS_Console("Sentinel quitting.\n");
     return 0;
@@ -91,7 +92,7 @@ P1_Join(int *pid, int *status)
     //     if there are children but no children have quit
     //        P1SetState(P1_STATE_JOINING)
     //        P1Dispatch(FALSE)
-    // until either a child quit or there are no more children
+    // until either a child quit or there are no children
     return result;
 }
 
