d81f5a508b9be9c1fbc556f7e1a1f87b445a9c8e
John H. Hartman <john.hartman@gmail.com>
2021-02-19 08:53:03 -0700

    updated pseudo-code

--- phase1d/phase1d.c
+++ phase1d/phase1d.c
@@ -11,20 +11,22 @@ static void DeviceHandler(int type, void *arg);
 static void SyscallHandler(int type, void *arg);
 static void IllegalInstructionHandler(int type, void *arg);
 
-static int sentinel(void *arg);
+static int Sentinel(void *arg);
 
 void 
 startup(int argc, char **argv)
 {
     int pid;
+    P1ProcInit();
+    P1LockInit();
     P1CondInit();
 
     // initialize device data structures
-    // put device interrupt handlers into interrupt vector
+    // put DeviceHandler into interrupt vector for the devices
     USLOSS_IntVec[USLOSS_SYSCALL_INT] = SyscallHandler;
 
     /* create the sentinel process */
-    int rc = P1_Fork("sentinel", sentinel, NULL, USLOSS_MIN_STACK, 6 , &pid);
+    int rc = P1_Fork("Sentinel", Sentinel, NULL, USLOSS_MIN_STACK, 6 , &pid);
     assert(rc == P1_SUCCESS);
     // should not return
     assert(0);
@@ -38,8 +40,12 @@ P1_DeviceWait(int type, int unit, int *status)
     int     result = P1_SUCCESS;
     // disable interrupts
     // check kernel mode
-    // use condition variable to wait for interrupt
-    // set *status to device's status
+    // acquire device's lock 
+    // while interrupt has not occurred and not aborted
+    //      wait for interrupt on type:unit
+    // if not aborted
+    //      set *status to device's status
+    // release lock
     // restore interrupts
     return result;
 }
@@ -48,26 +54,27 @@ P1_DeviceWait(int type, int unit, int *status)
 static void
 DeviceHandler(int type, void *arg) 
 {
+    // record that interrupt occurred
     // if clock device
-    //      P1_WakeupDevice every 5 ticks
+    //      naked signal clock every 5 ticks
     //      P1Dispatch(TRUE) every 4 ticks
     // else
-    //      P1_WakeupDevice
+    //      naked signal type:unit
 }
 
 static int
-sentinel (void *notused)
+Sentinel (void *notused)
 {
     int     pid;
     int     rc;
 
     /* start the P2_Startup process */
-    rc = P1_Fork("P2_Startup", P2_Startup, NULL, 4 * USLOSS_MIN_STACK, 1, &pid);
+    rc = P1_Fork("P2_Startup", P2_Startup, NULL, 4 * USLOSS_MIN_STACK, 2, &pid);
     assert(rc == P1_SUCCESS);
 
     // enable interrupts
     // while sentinel has children
-    //      get children that have quit via P1GetChildStatus (either tag)
+    //      use P1GetChildStatus to get children that have quit 
     //      wait for an interrupt via USLOSS_WaitInt
     USLOSS_Console("Sentinel quitting.\n");
     return 0;
@@ -78,11 +85,11 @@ P1_Join(int *pid, int *status)
 {
     int result = P1_SUCCESS;
     // disable interrupts
-    // kernel mode
-    // do
-    //     use P1GetChildStatus to get a child that has quit  
-    //     if no children have quit
-    //        set state to P1_STATE_JOINING vi P1SetState
+    // check kernel mode
+    // repeat
+    //     call P1GetChildStatus  
+    //     if there are children but no children have quit
+    //        P1SetState(P1_STATE_JOINING)
     //        P1Dispatch(FALSE)
     // until either a child quit or there are no more children
     return result;
@@ -91,8 +98,7 @@ P1_Join(int *pid, int *status)
 static void
 SyscallHandler(int type, void *arg) 
 {
-    USLOSS_Console("System call %d not implemented.\n", (int) arg);
-    USLOSS_IllegalInstruction();
+    USLOSS_Sysargs *sysargs = (USLOSS_Sysargs *) arg;
+    USLOSS_Console("System call %d not implemented.\n", sysargs->number);
+    P1_Quit(1025);
 }
-
-void finish(int argc, char **argv) {}
